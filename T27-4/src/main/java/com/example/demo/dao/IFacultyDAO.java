1aa8eb7c873a60a89c9b33c89e5ac94ff33e837e
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-22 11:51:08 +0200

    Delete T27-4 directory

--- T27-4/src/main/java/com/example/demo/dao/IFacultyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Faculty;
-
-public interface IFacultyDAO extends JpaRepository<Faculty, Long>{
-
-}
--- T27-4/src/main/java/com/example/demo/dao/ITeamDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Team;
-
-public interface ITeamDAO extends JpaRepository<Team, Long>{
-
-}
